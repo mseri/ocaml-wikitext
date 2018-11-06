d47deed90d7b0e71275a2e671b0a70f8c577454a
Maxence Abela <maxence.abela@geneanet.org>
2018-11-06 14:53:25 +0100

    [ADD] gitignore + types for blocks like Header, paragraphs, lists, etc...

--- wktxt_type.ml
+++ wktxt_type.ml
@@ -1,8 +1,26 @@
 type document = fragment list
 [@@deriving show]
 
+(*
+	Un block peut contenir des inlines mais pas d'autres blocks
+	Un inline peut être vide ou contenir un autre inline.
+
+	Les blocs identifiés ici sont :
+		Header, List, Numlist, Paragraph, Definition, Blockquote, (Table, align?)
+	
+	Les inlines sont par exemple les chaines de caractères Bold, italic, ou smallcaps, etc
+*)
+
 and fragment =
   | Header of int * fragment list
+  | List of fragment list 
+  | Numlist of int * fragment list 
+  | Paragraph of fragment list
+  | Definition of fragment list * fragment list
+  | Blockquote of fragment list
+  | Table of fragment list list
+  | Italic of fragment list
+  | Bold of fragment list
   | String of string
   | Char of char
 [@@deriving show]
