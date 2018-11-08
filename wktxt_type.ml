7a6d60c7e944a996300257cba2e61c111401b9e8
Maxence Abela <maxence.abela@geneanet.org>
2018-11-08 11:42:35 +0100

    fixed list, numlist and definition types

--- wktxt_type.ml
+++ wktxt_type.ml
@@ -11,24 +11,30 @@ type document = block list
 	Les inlines sont par exemple les chaines de caractères Bold, italic, ou smallcaps, etc
 
 	renommer "inline" en "block" et créer un type "inline" ?
+
+	Ces types sont ceux que l'on retrouvera dans l'AST.
 *)
 
 and block =
   | Header of int * inline list
-  | List of inline list 
+  | List of int * inline list
   | Numlist of int * inline list 
   | Paragraph of inline list
-  | Definition of inline list * inline list
+  | Definition of inline list
   | Blockquote of inline list
-  | Table of inline list list
+  | Table of inline list
 [@@deriving show]
 
 and inline = 
   | Italic of inline list
   | Bold of inline list
   | Smallcaps of inline list
+  | List_content of inline list
+  | Numlist_content of inline list
+  | Term_content of inline list
+  | Def_content of inline list
+  | Table_content of inline list
   | String of string 
-  | Char of char
 [@@deriving show]
 (* [@@deriving show] va créer automatique les fonctions
    [show_document], [show_inline], ... *)
